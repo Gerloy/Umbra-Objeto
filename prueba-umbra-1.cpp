dbe6223daa4fc62cc7ef17a0e117608d0c53816f
Gerloy <gersilro@hotmail.com>
2025-06-27 20:50:46 -0300

    Para pasar a notebook
    dwadwa

--- prueba-umbra-1.cpp
+++ prueba-umbra-1.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "Servo.h"
+#include "servo/Servo.h"
+#include "osc/PicoOSC.hpp"
 
-#define BOTON 1
+/*#define BOTON 1
 #define SERVO_PIN 2
-#define DELAY 1000
+#define DELAY 1000*/
 
 int main()
 {
-    stdio_init_all();
+    /*stdio_init_all();
     gpio_init(BOTON);
     gpio_set_dir(BOTON, GPIO_IN);
     Servo servo(SERVO_PIN);
@@ -19,6 +20,6 @@ int main()
             servo.goDegree(00.0);
             sleep_ms(DELAY);
         }
-    }
+    }*/
 
 }
